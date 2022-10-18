d2b6051af24cdcc47eb2f10f24a60e41ba3d903d
Fedou Brice <napofedoupro@gmail.com>
2022-10-18 15:00:30 +0200

    Fix logos Social

--- src/data/contact/socials.js
+++ src/data/contact/socials.js
@@ -1,25 +1,25 @@
 export const dataSocials = [
   {
     id: 1,
-    img: "linkedin.svg",
+    img: "linkedin.png",
     title: "Linkedin",
     link: "https://www.linkedin.com/in/brice-fedou-928b89190/",
   },
   {
     id: 2,
-    img: "github.svg",
+    img: "github.png",
     title: "Github",
     link: "https://github.com/Bricefed",
   },
   {
     id: 3,
-    img: "instagram.svg",
+    img: "instagram.png",
     title: "Instagram",
     link: "https://www.instagram.com/codeur369/",
   },
   {
     id: 4,
-    img: "malt.svg",
+    img: "malt.png",
     title: "Malt",
     link: "https://www.malt.fr/profile/bricefedou?overview=true",
   },
